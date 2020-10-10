5e02f369ac923a92be3dd44052879b4737b0c7c5
Ronie <roniemeque@icloud.com>
2020-10-09 21:05:49 -0300

    create and fetch all messages

---
+++ api/messages/index.ts
@@ -0,0 +1,28 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import handleCors from "../../src/lib/cors";
+import { allItemsByIndex, createItem } from "../../src/lib/fauna";
+
+export default async (req: NowRequest, res: NowResponse) => {
+  if (handleCors(req, res)) return;
+
+  const { method, body } = req;
+
+  if (method === "GET") {
+    const items = await allItemsByIndex("all_messages");
+
+    if (!items)
+      return res.status(500).json({ message: "Something went wrong" });
+
+    return res.status(200).json({ items });
+  }
+
+  if (method === "POST") {
+    const item = await createItem("messages", body);
+
+    if (!item) return res.status(500).json({ message: "Something went wrong" });
+
+    return res.status(200).json({ item });
+  }
+
+  res.status(405).json({ message: "Method not available" });
+};
